5b472f8301d43e2f9f0e11a0cd89535827261a89
mengkzhaoyun <mengkzhaoyun@gmail.com>
2016-05-12 19:37:02 +0800

    Add Fis Practise Sample 01

---
+++ 05.fis01/fis-conf.js
@@ -0,0 +1,41 @@
+﻿fis.config.init({
+    project: {
+        md5Length: 7
+    }
+});
+
+fis.config.merge({
+    roadmap: {
+        path: [
+            {
+                //查找所有Assets目录下的所有文件
+                reg: /^\/Libs\/(.*)/i,
+                //生成新的url '/Static/xxxx'
+                url: '/Static/$1',
+                //指定文件不产出MD5戳
+                useHash: false,
+                release: '/Static/$1'
+            },
+            {
+                //查找所有Assets目录下的js,css,png文件
+                reg: /^\/Assets\/(.*\.(?:js|css|png))/i,
+                //生成新的url '/myappAssets/xxxx'
+                url: '/myappAssets/$1',
+                //js/css/png打上md5标记生成到/myappAssets/xxx目录下
+                release: '/myappAssets/$1'
+            },            
+            {
+                //所有根目录下的.html文件
+                reg : /^(.*\.html)/i,
+                //发布到/myapp/xxx目录下
+                release : '/myapp/$1'
+            },
+            {
+                //前面规则未匹配到的其他文件
+                reg: /.*/,
+                //编译的时候不要产出了
+                release: false
+            }
+        ]
+    }
+});
